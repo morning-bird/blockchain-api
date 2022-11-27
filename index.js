065655b61523de0c40db8bd8b7c2a3e641c307a8
Renato - Local Server <renato.budiman07@gmail.com>
2022-11-27 01:36:08 +0000

    update

--- index.js
+++ index.js
@@ -1,26 +1,27 @@
-require('dotenv').config()
-const express = require('express')
-const iotx = require("./modules/iotxBlockchain")
-const app = express()
-const port = 7000
+require("dotenv").config();
+const express = require("express");
+const iotx = require("./modules/iotxBlockchain");
+const app = express();
+const port = 7000;
 
-app.get('/wallet/balance', async (req, res) => {
-    const balance = await iotx.getBalance();
-    const result = {
-        balance: balance
-    }
-    return res.json(result)
-})
+app.get("/wallet/balance", async (req, res) => {
+  const balance = await iotx.getBalance();
+  const result = {
+    balance: balance,
+  };
+  return res.json(result);
+});
 
-app.get('/block/height', async (req, res) => {
-    const blockHeight = await iotx.getBlockHeight()
-    const synced = await iotx.isBlockHeightSynced()
-    return res.json({
-        blockHeight: blockHeight,
-        synced: synced
-    })
-})
+app.get("/block/height", async (req, res) => {
+  const { synced, localRPCBlockHeight, publicRPCBlockHeight } =
+    await iotx.isBlockHeightSynced();
+  return res.json({
+    blockHeight: localRPCBlockHeight,
+    publicRPCBlockHeight: publicRPCBlockHeight,
+    synced: synced,
+  });
+});
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
--- modules/iotxBlockchain.js
+++ modules/iotxBlockchain.js
@@ -1,31 +1,34 @@
-const { ethers } = require("ethers")
-console.log(process.env.LOCAL_RPC)
-const provider = new ethers.providers.JsonRpcProvider(process.env.LOCAL_RPC)
+const { ethers } = require("ethers");
+console.log(process.env.LOCAL_RPC);
+const provider = new ethers.providers.JsonRpcProvider(process.env.LOCAL_RPC);
 
 const getBalance = async () => {
-    const balance = await provider.getBalance(process.env.WALLET_ADDRESS)
-    return ethers.utils.formatEther(balance)
-}
+  const balance = await provider.getBalance(process.env.WALLET_ADDRESS);
+  return ethers.utils.formatEther(balance);
+};
 
 const getBlockHeight = async (rpc = "") => {
-    let result = ""
-    if (rpc === "") {
-        result = await provider.getBlockNumber()
-    } else {
-        const provider = new ethers.providers.JsonRpcProvider(rpc)
-        result = await provider.getBlockNumber()
-    }
-    return result
-}
+  let result = "";
+  if (rpc === "") {
+    result = await provider.getBlockNumber();
+  } else {
+    const provider = new ethers.providers.JsonRpcProvider(rpc);
+    result = await provider.getBlockNumber();
+  }
+  return result;
+};
 
 const isBlockHeightSynced = async () => {
-    const localRPCBlockHeight = await getBlockHeight()
-    const publicRPCBlockHeight = await getBlockHeight(process.env.PUBLIC_RPC)
-    let isOkay = false
-    if (localRPCBlockHeight == publicRPCBlockHeight || publicRPCBlockHeight - 3 < localRPCBlockHeight) {
-        isOkay = true
-    }
-    return isOkay
-}
+  const localRPCBlockHeight = await getBlockHeight();
+  const publicRPCBlockHeight = await getBlockHeight(process.env.PUBLIC_RPC);
+  let synced = false;
+  if (
+    localRPCBlockHeight == publicRPCBlockHeight ||
+    publicRPCBlockHeight - 3 < localRPCBlockHeight
+  ) {
+    synced = true;
+  }
+  return { synced, localRPCBlockHeight, publicRPCBlockHeight };
+};
 
-module.exports = { getBalance, getBlockHeight, isBlockHeightSynced }
+module.exports = { getBalance, getBlockHeight, isBlockHeightSynced };
